8b3743c5914073c88f84ebcd0a1f50d49663d69f
Harsh Gadhiya <harshgadhiya8@gmail.com>
2024-02-05 16:37:05 +0530

    Day 24 done

---
+++ day24/main.py
@@ -0,0 +1,7 @@
+# with open("file.txt") as file:
+#     contents = file.read()
+#     a = int(contents)
+#     print(type(a))
+
+with open("C:/Users/Harsh/Desktop/new_file.txt",mode='w') as file:
+    file.write("Harsh")
